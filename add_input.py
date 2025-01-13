5b2a84bb0012902e32310eb275a87cbb918e6ea5
Green <rusman_ml@mail.ru>
2025-01-13 22:59:52 +0700

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,6 @@
+username = input("Введите имя")
+title = input("Введите заголовок заметки")
+content = input("Введите описание заметки")
+status = input("Введите статус заметки")
+created_date = input("Введите дату в формате Д-М-Г")
+issue_date = input("Введите дату в формате Д-М-Г")
