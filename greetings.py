91ae1fdd1feb5cd54a3014253d1c8ee714bfebf8
Green <rusman_ml@mail.ru>
2025-01-13 19:23:53 +0700

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Yan"
+title = "Grade 1"
+content = "Stage 1"
+status = "at work"
+created_date = "10-01-2025"
+issue_date = "10-02-2025"
+print("Yan:", username)
+print("Grade 1", title)
+print("Stage 1", content)
+print("at work", status)
+print("10-01-2025", created_date)
+print("10-02-2025", issue_date)
