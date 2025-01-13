57907814fa034a39f3a42d486a3dfd443a90ba03
Green <rusman_ml@mail.ru>
2025-01-13 20:37:59 +0700

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -4,9 +4,9 @@ content = "Stage 1"
 status = "at work"
 created_date = "10-01-2025"
 issue_date = "10-02-2025"
-print("Yan:", username)
-print("Grade 1", title)
-print("Stage 1", content)
-print("at work", status)
-print("10-01-2025", created_date)
-print("10-02-2025", issue_date)
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания:", created_date)
+print("Дата истечения:", issue_date)
