6c28153fa5704c21b655878a421fea504649fb2f
Green <rusman_ml@mail.ru>
2025-01-20 20:14:24 +0700

    Add files via upload

---
+++ final.py
@@ -0,0 +1,10 @@
+user_name = input("Введите имя пользователя")
+content = input("Введите описание вашей заметки")
+status = input("Введите статус заметки")
+created_date = input("Введите дату создания заметки в формате дд.мм")
+issue_date = input("Введите планируемую дату окончания заметки в формате дд.мм")
+title1 = input("Введите первый заголовок")
+title2 = input("Введите второй заголовок")
+title3 = input("введите третий заголовок")
+note = [user_name, content, status, created_date, issue_date, [title1, title2, title3]]
+print(note)
