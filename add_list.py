96fb8122d0a88c1b14a571b9a466d49f04a826fe
Green <rusman_ml@mail.ru>
2025-01-23 22:09:00 +0700

    Add files via upload

--- add_list.py
+++ add_list.py
@@ -1,5 +1,6 @@
-title1 = input("Введите первый заголовок")
-title2 = input("Введите второй заголовок")
-title3 = input("Введите третий заголовок")
-title123 = title1, title2, title3
+
+title1 = input("Введите первый заголовок: ")
+title2 = input("Введите второй заголовок: ")
+title3 = input("Введите третий заголовок: ")
+title123 = [title1, title2, title3]
 print(title123)
